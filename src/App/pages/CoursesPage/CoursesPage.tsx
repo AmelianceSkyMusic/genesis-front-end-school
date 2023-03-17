0a39f5baebf08c4b2585ef2ce484a4ed0f3389dd
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-17 18:23:54 +0200

    ^ update CoursesPage • add pagination • add preloader • add error handler

--- src/App/pages/CoursesPage/CoursesPage.tsx
+++ src/App/pages/CoursesPage/CoursesPage.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 
-import type { GetCoursesResponse } from '~api/getCourses';
 import { api } from '~api/index';
+import type { Course } from '~api/types/Courses';
 import type { ErrorResponse } from '~api/types/ErrorResponse';
+import { ErrorPlaceholder } from '~components/ErrorPlaceholder/ErrorPlaceholder';
+import { Pagination } from '~components/Pagination/Pagination';
 
+import { LoaderOverlay } from '~/ameliance-ui/components/_LAB/LoaderOverlay';
 import { Block } from '~/ameliance-ui/components/blocks/Block';
 import { Grid } from '~/ameliance-ui/components/Grid';
 import { Typography } from '~/ameliance-ui/components/Typography';
@@ -12,8 +15,13 @@ import { CourseCard } from './CourseCard/CourseCard';
 
 import s from './CoursesPage.module.scss';
 
+const firstPageNumber = 0;
+const coursesPerPage = 10;
+
 export function CoursesPage() {
-	const [state, setState] = useState<GetCoursesResponse | ErrorResponse>();
+	const [state, setState] = useState<{ courses: Course[][]; status: 'success' } | ErrorResponse>();
+	const [currentPage, setCurrentPage] = useState(0);
+	const [lastPageNumber, setLastPageNumber] = useState(1);
 
 	useEffect(() => {
 		const dataFetch = async () => {
@@ -22,7 +30,13 @@ export function CoursesPage() {
 			if (typeof error !== 'string') error = 'unknown';
 
 			if ('courses' in response) {
-				setState({ courses: response.courses.reverse(), status: response.status });
+				const courses = response.courses.reverse();
+				const coursesPages = [];
+				for (let i = 0; i < courses.length; i += coursesPerPage) {
+					coursesPages.push(courses.slice(i, i + coursesPerPage));
+				}
+				setLastPageNumber(coursesPages.length - 1);
+				setState({ courses: coursesPages, status: response.status });
 			} else {
 				setState({ error: response.error, status: response.status });
 			}
@@ -31,29 +45,47 @@ export function CoursesPage() {
 		dataFetch();
 	}, []);
 
+	if (state?.status === 'error' && typeof state.error === 'string') {
+		return (
+			<ErrorPlaceholder>
+				<Typography component="p1">{state.error}</Typography>
+			</ErrorPlaceholder>
+		);
+	}
+
 	return (
 		<Block component="main" className={s.CoursesPage}>
 			<Grid container component="section" className={s.container}>
-				<Typography component="h2">Courses</Typography>
-				<Grid row component="section" className={s.courses}>
-					{state?.status === 'success' && (
-						state.courses.map((course) => (
-							<CourseCard
-								key={course.id}
-								title={course.title}
-								previewImg={course.previewImageLink}
-								video={course.meta?.courseVideoPreview?.link || ''}
-								description={course.description || ''}
-								lessonsCount={course.lessonsCount}
-								skills={course.tags}
-								rating={course.rating}
-								className="col-xx-6 col-md-12"
-							/>
-						))
-					)}
-					{!state?.status && <Typography component="h1">LOADING...</Typography>}
-					{state?.status === 'error' && typeof state.error === 'string' && <Typography component="h1">{state.error}</Typography>}
-				</Grid>
+				{state?.status === 'success' && (
+					<>
+						<Typography component="h2">Courses</Typography>
+						<Grid row component="section" className={s.courses}>
+							{
+								state.courses[currentPage].map((course) => (
+									<CourseCard
+										key={course.id}
+										id={course.id}
+										title={course.title}
+										previewImg={course.previewImageLink}
+										video={course.meta?.courseVideoPreview?.link || ''}
+										description={course.description || ''}
+										lessonsCount={course.lessonsCount}
+										skills={course.tags}
+										rating={course.rating}
+										className="col-xx-6 col-md-12"
+									/>
+								))
+							}
+						</Grid>
+						<Pagination
+							setCurrentPage={setCurrentPage}
+							currentPageNumber={currentPage}
+							firstPageNumber={firstPageNumber}
+							lastPageNumber={lastPageNumber}
+						/>
+					</>
+				)}
+				{!state?.status && <LoaderOverlay />}
 			</Grid>
 		</Block>
 	);
