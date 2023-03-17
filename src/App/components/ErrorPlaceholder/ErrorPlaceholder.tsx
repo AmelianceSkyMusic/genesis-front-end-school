4ab87553192c6954ec8db359c4880564cc74fa07
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-17 18:21:57 +0200

    = add ErrorPlaceholder component

---
+++ src/App/components/ErrorPlaceholder/ErrorPlaceholder.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from 'react';
+
+import { Block } from '~/ameliance-ui/components/blocks/Block';
+import { Grid } from '~/ameliance-ui/components/Grid';
+import { Typography } from '~/ameliance-ui/components/Typography';
+
+import s from './ErrorPlaceholder.module.scss';
+
+interface ErrorPlaceholder {
+	children: ReactNode;
+}
+
+export function ErrorPlaceholder({ children }: ErrorPlaceholder) {
+	return (
+		<Block component="main" className={s.ErrorPlaceholder}>
+			<Grid container component="section" className={s.container}>
+				<Typography component="h4">ERROR:</Typography>
+				{children}
+			</Grid>
+		</Block>
+	);
+}
