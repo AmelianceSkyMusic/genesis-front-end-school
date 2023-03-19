08b7357ab2ca92fb5e0ac5c7d2ee924f98a286bf
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-19 14:45:09 +0200

    * update path to constant variable

--- src/App/components/Logo.tsx
+++ src/App/components/Logo.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import { ROUTES } from '~constants/ROUTES';
+
 import { WiseyLogo } from './SVG/WiseyLogo';
 
 export function Logo() {
 	return (
-		<Link to="/">
+		<Link to={`${ROUTES.COURSES}/1`}>
 			<WiseyLogo size="custom" style={{ width: '100px', height: '30px' }} />
 		</Link>
 	);
