2e738c080f0c5e7606807590b7a89519b8bbc3ba
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-24 17:13:34 +0200

    * update asmlib • refactor types

--- src/ameliance-ui/components/blocks/Block.tsx
+++ src/ameliance-ui/components/blocks/Block.tsx
@@ -4,8 +4,8 @@ import { forwardRef } from 'react';
 import asm from 'asm-ts-scripts';
 
 import { Component } from '../_LAB/Component';
-import type { Grid } from './helpers/grid';
 import { getGridClass } from './helpers/grid';
+import type { Grid } from './types/Grid';
 
 type ComponentElementType = ComponentProps<ElementType>;
 
